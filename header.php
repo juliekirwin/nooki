830a6c42e76a935c2f2ff77a9bf6840bc58a55d2
Nolan Vance <nolanvance18@gmail.com>
2013-07-27 16:03:03 -0400

    asdad

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 function Echo_Header()
 {	
 	echo '<a href="http://127.0.0.1/projects/nooki/index.php"><img src="http://127.0.0.1/projects/nooki/images/logo.png"></img></a>
-		<p>nooki</p>';
+		<p>&nbsp;</p>';
 }
 
 
